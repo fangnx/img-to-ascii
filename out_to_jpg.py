fda2b78f91c90cacb88fdf305bfbbb5fa2083769
fnx <fnx@fnxdeMacBook-Pro.local>
2018-10-15 17:29:35 -0400

    Update out_to_jpg

--- out_to_jpg.py
+++ out_to_jpg.py
@@ -1,29 +1,32 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 from to_ascii import *
 
 
 def main():
+    img_density_factor = 6
+
     img_name = './test_jiang.jpg'
-    out_jpg_name = file_name_ignore_extension(img_name) + '_in_ascii.jpg'
-    init_img = Image.open(img_name)
-    width, height = init_img.size
+    in_img = Image.open(img_name)
+    width, height = in_img.size
 
-    file_out(img_pixels_to_asciis(init_img), out_jpg_name, width, height)
+    out_img = Image.new(mode='RGB',
+                        size=(width * img_density_factor, height * img_density_factor),
+                        color=(255, 255, 255))
 
+    asciis_to_img(in_img, out_img, img_density_factor, width, height)
 
-def asciis_to_jpg(txt_file)：
-        
-
-
-
+    out_img.save((file_name_ignore_extension(img_name) + '_in_ascii.jpg'))
 
 
+def asciis_to_img(in_img, out_img, density, width, height):
+    draw = ImageDraw.Draw(out_img)
+    ascii_list = img_pixels_to_asciis(in_img)
 
+    for h in range(0, height):
+        for w in range(0, width):
+            draw.text(xy=(w * density, h * density), text=''.join(ascii_list[h*width + w]), fill=(0, 0, 0))
 
 
 if __name__ == '__main__':
     main()
-
-
-
 
--- out_to_txt.py
+++ out_to_txt.py
@@ -2,15 +2,15 @@ from to_ascii import *
 
 
 def main():
-    img_name = './test_jiang.jpg'
+    img_name = './test_bao.jpeg'
     out_txt_name = file_name_ignore_extension(img_name) + '_in_ascii_compressed.txt'
-    init_img = Image.open(img_name)
-    width, height = init_img.size
+    img = Image.open(img_name)
+    width, height = img.size
     height *= 0.5
 
-    scale_factor = 0.2
+    scale_factor = 0.4
     new_width, new_height = int(width * scale_factor), int(height * scale_factor)
-    scaled_img = rescale_img(init_img, new_width, new_height)
+    scaled_img = rescale_img(img, new_width, new_height)
 
     txt_out(img_pixels_to_asciis(scaled_img), out_txt_name, new_width, new_height)
 
